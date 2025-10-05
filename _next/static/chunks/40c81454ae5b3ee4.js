c8e7f4a4750242403b3464aa73d8b60e3aa18021
Azim Memon <memonazim2002@gmail.com>
2025-10-05 14:29:53 +0530

    Updates

---
+++ _next/static/chunks/40c81454ae5b3ee4.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/5e0c29652bb21467.js",
+  "static/chunks/turbopack-d04b393451a23311.js"
+])
---
+++ _next/static/xZXUyB7WBnqrLumJUEJDc/_buildManifest.js
@@ -0,0 +1,14 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/16bdb319302e2ccb.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
